dad1bb859c15aa52afb6e4d2015a552381b6ff2a
Roberto Esparza <phd.roberto.esparza@gmail.com>
2025-11-12 20:26:09 -0600

    Create login_page.py

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+from pages.base_page.py import BasePage
+
+class LoginPage(BasePage):
+    URL = "https://the-internet.herokuapp.com/login"
+    USERNAME_INPUT = (By.ID, "username")
+    PASSWORD_INPUT = (By.ID, "password")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "button.radius")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".flash.success")
+
+    def open(self):
+        self.open_url(self.URL)
+
+    def login(self, username, password):
+        self.send_keys(self.USERNAME_INPUT, username)
+        self.send_keys(self.PASSWORD_INPUT, password)
+        self.click(self.LOGIN_BUTTON)
+
+    def is_success_message_displayed(self):
+        return self.is_element_displayed(self.SUCCESS_MESSAGE)
