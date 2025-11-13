255a6787ede8dd0160447ba68431c3dde4265134
Roberto Esparza <phd.roberto.esparza@gmail.com>
2025-11-12 20:29:52 -0600

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,21 @@
+import logging
+import os
+
+def create_logger(name="SeleniumFramework"):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        # Create console handler
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+
+        # Create formatter
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        ch.setFormatter(formatter)
+
+        logger.addHandler(ch)
+
+    return logger
